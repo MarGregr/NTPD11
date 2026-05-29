58412a90e9ec58d83d84d5bfd5adbdef67a4ac75
MarGregr <marcingregr@gmail.com>
2026-05-29 21:27:50 +0200

    Zad05 Correction

--- Zad05_odczyt.py
+++ Zad05_odczyt.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pyspark.sql import SparkSession
 
-# Konfiguracja środowiska Spark dla Windows
+#Konfiguracja środowiska Spark dla Windows
 os.environ['HADOOP_HOME'] = "C:/hadoop"
 os.environ['PATH'] = os.environ['PATH'] + ";C:/hadoop/bin"
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--driver-java-options "-Djava.security.manager=allow" pyspark-shell'
@@ -10,7 +10,7 @@ os.environ['PYSPARK_SUBMIT_ARGS'] = '--driver-java-options "-Djava.security.mana
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-# Inicjalizacja tradycyjnej (statycznej) sesji Spark dla przetwarzania Batch
+#Inicjalizacja tradycyjnej (statycznej) sesji Spark dla przetwarzania Batch
 spark = SparkSession.builder \
     .appName("LAB11_Zadanie5_Weryfikacja_Batch") \
     .master("local[*]") \
@@ -21,7 +21,6 @@ spark.sparkContext.setLogLevel("WARN")
 print("Uruchamianie odczytu:")
 
 try:
-    # Wskazanie "*.parquet" zmusza Sparka do zaczytania samych plików z pominięciem usterki w metadanych
     batch_df = spark.read.parquet("data/output_stream/*.parquet")
 
     print(f"\n[SUKCES] Liczba zapisanych wierszy w plikach Parquet: {batch_df.count()}")
